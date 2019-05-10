b05ba6714e3815974628ff871b047baf832ca5bc
wearewho <wearewh555o@gmail.com>
2019-05-11 00:24:03 +0700

    edit font and footer

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -24,7 +24,7 @@
                                 <div class="wdgt-pst-wrp">
                                     @if (!empty($footer[1][0]))
                                         <div class="wdgt-pst">
-                                            <a href="{{ route('blog-detail',[$footer[1][0]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][0]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][0]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                            <a href="{{ route('blog-detail',[$footer[1][0]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][0]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][0]->imageblog[0]->image_name }}" itemprop="image"></a>
                                             <div class="wdgt-inf">
                                                 <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][0]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][0]->header, 0, 35) }}...</a></h4>
                                                 <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][0]->created_at)) }}</span>
@@ -34,7 +34,7 @@
                                     
                                     @if (!empty($footer[1][1]))
                                         <div class="wdgt-pst">
-                                            <a href="{{ route('blog-detail',[$footer[1][1]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][1]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][1]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                            <a href="{{ route('blog-detail',[$footer[1][1]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][1]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][1]->imageblog[0]->image_name }}" itemprop="image"></a>
                                             <div class="wdgt-inf">
                                                 <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][1]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][1]->header, 0, 35) }}...</a></h4>
                                                 <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][1]->created_at)) }}</span>
@@ -44,7 +44,7 @@
                                     
                                     @if (!empty($footer[1][2]))
                                         <div class="wdgt-pst">
-                                            <a href="{{ route('blog-detail',[$footer[1][2]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][2]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][2]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                            <a href="{{ route('blog-detail',[$footer[1][2]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][2]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][2]->imageblog[0]->image_name }}" itemprop="image"></a>
                                             <div class="wdgt-inf">
                                                 <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][2]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][2]->header, 0, 35) }}...</a></h4>
                                                 <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][2]->created_at)) }}</span>
@@ -60,7 +60,7 @@
                                 <br>
                                 @if (!empty($footer[1][3]))
                                     <div class="wdgt-pst">
-                                        <a href="{{ route('blog-detail',[$footer[1][3]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][3]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][3]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                        <a href="{{ route('blog-detail',[$footer[1][3]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][3]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][3]->imageblog[0]->image_name }}" itemprop="image"></a>
                                         <div class="wdgt-inf">
                                             <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][3]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][3]->header, 0, 35) }}...</a></h4>
                                             <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][3]->created_at)) }}</span>
@@ -70,7 +70,7 @@
                                 
                                 @if (!empty($footer[1][4]))
                                     <div class="wdgt-pst">
-                                        <a href="{{ route('blog-detail',[$footer[1][4]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][4]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][4]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                        <a href="{{ route('blog-detail',[$footer[1][4]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][4]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][4]->imageblog[0]->image_name }}" itemprop="image"></a>
                                         <div class="wdgt-inf">
                                             <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][4]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][4]->header, 0, 35) }}...</a></h4>
                                             <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][4]->created_at)) }}</span>
@@ -80,7 +80,7 @@
                                 
                                 @if (!empty($footer[1][5]))
                                     <div class="wdgt-pst">
-                                        <a href="{{ route('blog-detail',[$footer[1][5]->id]) }}" title="" itemprop="url"><img src="{{ $footer[1][5]->imageblog[0]->image_url }}" width="60" height="60" alt="{{ $footer[1][5]->imageblog[0]->image_name }}" itemprop="image"></a>
+                                        <a href="{{ route('blog-detail',[$footer[1][5]->id]) }}" title="" itemprop="url"><img src="{{ url($footer[1][5]->imageblog[0]->image_url) }}" width="60" height="60" alt="{{ $footer[1][5]->imageblog[0]->image_name }}" itemprop="image"></a>
                                         <div class="wdgt-inf">
                                             <h4 itemprop="headline"><a href="{{ route('blog-detail',[$footer[1][5]->id]) }}" title="" itemprop="url">{{  mb_substr($footer[1][5]->header, 0, 35) }}...</a></h4>
                                             <span><i class="fa fa-clock-o"></i>{{ date("d/m/Y", strtotime($footer[1][5]->created_at)) }}</span>
@@ -96,16 +96,16 @@
                                     <div class="wdgt-gal-itm">
                                         <div class="row mrg">
                                             @if (!empty($footer[2][0]))
-                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][0]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][0]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][0]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][0]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                             @endif
                                             @if (!empty($footer[2][1]))
-                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][1]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][1]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][1]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][1]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                             @endif
                                             @if (!empty($footer[2][2]))
-                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][2]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][2]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][2]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][2]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                             @endif
                                             @if (!empty($footer[2][3]))
-                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][3]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][3]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][3]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][3]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                             @endif
                                         </div>
                                     </div>
@@ -113,16 +113,16 @@
                                         <div class="wdgt-gal-itm">
                                             <div class="row mrg">
                                                 @if (!empty($footer[2][4]))
-                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][4]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][4]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][4]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][4]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                                 @endif
                                                 @if (!empty($footer[2][5]))
-                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][5]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][5]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][5]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][5]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                                 @endif
                                                 @if (!empty($footer[2][6]))
-                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][6]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][6]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][6]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][6]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                                 @endif
                                                 @if (!empty($footer[2][7]))
-                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ $footer[2][7]->imagegallery[0]->image_url }}" width="135" height="104" alt="{{ $footer[2][7]->imagegallery[0]->image_name }}" itemprop="image"></div>
+                                                    <div class="col-md-6 col-sm-6 col-lg-6"><img src="{{ url($footer[2][7]->imagegallery[0]->image_url) }}" width="135" height="104" alt="{{ $footer[2][7]->imagegallery[0]->image_name }}" itemprop="image"></div>
                                                 @endif
                                             </div>
                                         </div>
